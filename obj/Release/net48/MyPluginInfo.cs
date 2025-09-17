6b65db1898d43b44ab81e0c92ff52d67e47bbf0b
Little-Sparroh <wingedoracle@tutanota.com>
2025-09-17 19:40:56 +0000

    Delete obj/Release/net48 directory

--- obj/Release/net48/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace StatReformat
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "StatReformat";
-        public const string PLUGIN_NAME = "StatReformat";
-        public const string PLUGIN_VERSION = "..";
-    }
-}
